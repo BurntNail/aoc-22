8f4623eb61f0ccea5c851846e11256ed860bec8f
BurntNail <BurntNail@proton.me>
2022-12-05 22:42:31 +0000

    clean up d5

--- crates/day5/src/main.rs
+++ crates/day5/src/main.rs
@@ -9,27 +9,24 @@ peg::parser!(grammar movement() for str {
 use crates_list::crates;
 use movement::movement;
 
-fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+use color_eyre::{
+    eyre::{bail, eyre},
+    Result,
+};
 
-    let input = include_str!("input.txt")
-        .lines()
-        .map(|l| l.trim_end())
-        .collect::<Vec<_>>();
-
-    let no_crate_rows = {
-        let mut ans = None;
-        for (i, l) in input.iter().enumerate() {
-            if l.contains(" 1") {
-                ans = Some(i);
-                break;
-            } else if l.contains("move") {
-                break; //uh oh
-            }
+fn find_crate_rows<'a>(input: impl Iterator<Item = &'a String>) -> Result<usize> {
+    for (i, l) in input.enumerate() {
+        if l.contains(" 1") {
+            return Ok(i);
+        } else if l.contains("move") {
+            break; //uh oh
         }
-        ans.expect("unable to find end of buckets")
-    };
+    }
 
+    bail!("unable to find end of buckets")
+}
+
+fn find_vertical_buckets(input: &[String], no_crate_rows: usize) -> Result<Vec<Vec<char>>> {
     let mut buckets: Vec<Vec<char>> = vec![Vec::with_capacity(no_crate_rows); 9];
     for crate_instr in &input[0..no_crate_rows] {
         let hb: Vec<Option<char>> = crates(crate_instr)?;
@@ -39,27 +36,47 @@ fn main() -> color_eyre::Result<()> {
             }
         }
     }
+    buckets.iter_mut().for_each(|b| b.reverse());
+    Ok(buckets)
+}
 
-    for b in buckets.iter_mut() {
-        b.reverse();
-    }
-
+fn execute_instructions(
+    buckets: &mut Vec<Vec<char>>,
+    input: &[String],
+    no_crate_rows: usize,
+    is_p1: bool,
+) -> Result<()> {
     for move_instr in &input[(no_crate_rows + 2)..] {
         let (no, from, to): (usize, usize, usize) = movement(move_instr)?;
+
         let mut taken = (0..no)
             .into_iter()
-            .map(|_| {
-                buckets[from]
-                    .pop()
-                    .ok_or_else(|| color_eyre::eyre::eyre!("Empty Stack"))
-            })
+            .map(|_| buckets[from].pop().ok_or_else(|| eyre!("Empty Stack")))
             .collect::<Result<Vec<_>, _>>()?;
-        // taken.reverse(); //For P1, comment this line
+
+        if is_p1 {
+            taken.reverse();
+        }
+
         buckets[to].append(&mut taken);
     }
 
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = include_str!("input.txt")
+        .lines()
+        .map(|l| l.trim_end().to_string())
+        .collect::<Vec<_>>();
+
+    let no_crate_rows = find_crate_rows(input.iter())?;
+    let mut buckets = find_vertical_buckets(&input, no_crate_rows)?;
+    execute_instructions(&mut buckets, &input, no_crate_rows, true)?;
+
     for mut b in buckets.into_iter() {
-        // let mut b: Vec<char> = b.into_iter().flatten().collect();
         print!("{}", b.pop().expect("element to pop"));
     }
 
