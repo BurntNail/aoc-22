a8ca858f8fb6f2599ee250467ca11149b7e2df83
BurntNail <BurntNail@proton.me>
2023-01-12 18:37:57 +0000

    working d9 sample

--- crates/day9/src/main.rs
+++ crates/day9/src/main.rs
@@ -16,46 +16,46 @@ fn main() {
         match instr {
             Direction::Up => {
                 hy -= 1;
-                if tx == hx {
-                    ty -= 1;
-                }
             }
             Direction::Down => {
                 hy += 1;
-                if tx == hx {
-                    ty += 1;
-                }
             }
             Direction::Left => {
                 hx -= 1;
-                if ty == hy {
-                    tx -= 1;
-                }
             }
             Direction::Right => {
                 hx += 1;
-                if ty == hy {
-                    tx += 1;
-                }
-
             }
-        }
-        if ((tx - hx) as f32).hypot((ty - hy) as f32) < 1.5 {
-            continue;
         }
 
         let dx = hx - tx;
         let dy = hy - ty;
 
         let (addx, addy) = match (dx, dy) {
-            (1,2) | (2,1) => (1,1),
-            (-1,2) | (-2,1) => (-1,1),
-            (1,-2) | (2,-1) => (1,-1),
-            (-1,-2) | (-2,-1) => (-1,-1),
+            (1, 2) | (2, 1) => (1, 1),
+            (-1, 2) | (-2, 1) => (-1, 1),
+            (1, -2) | (2, -1) => (1, -1),
+            (-1, -2) | (-2, -1) => (-1, -1),
+            (0, 2) => (0, 1),
+            (0, -2) => (0, -1),
+            (2, 0) => (1, 0),
+            (-2, 0) => (-1, 0),
+            (0, 0)
+            | (1, 0)
+            | (0, 1)
+            | (1, 1)
+            | (-1, 0)
+            | (0, -1)
+            | (-1, -1)
+            | (1, -1)
+            | (-1, 1) => (0, 0),
             _ => panic!("Unseen dx dy  - {},{}", dx, dy),
         };
+
         tx += addx;
         ty += addy;
+
+        println!("({tx},{ty})");
     }
 
     println!("{}", visited_locations.len());
