120cf5fc4b6e0d42a25968da109744c0e03e101d
BurntNail <BurntNail@proton.me>
2023-01-13 16:58:39 +0000

    add clippy warns

--- crates/day8/src/main.rs
+++ crates/day8/src/main.rs
@@ -1,8 +1,10 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
 use crate::map::Grid;
 
 mod map;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let input = include_str!("input.txt");
     let grid = Grid::from(input.to_string());
 
@@ -11,6 +13,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut scores = grid.scenic_scores();
     scores.sort();
     println!("Top Score: {:?}", scores.pop().unwrap());
-
-    Ok(())
 }
