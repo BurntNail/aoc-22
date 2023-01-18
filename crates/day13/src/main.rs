8d7e99ff5d0159c4db73e088e7709b9503ef84c6
Jack Maguire <jackmaguire1234@gmail.com>
2023-01-18 21:31:08 +0000

    almost d13 p2

--- crates/day13/src/main.rs
+++ crates/day13/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
     let input = include_str!("input.txt");
     let pairs = Pair::get_all(input).unwrap().1;
     println!("Working sum is {:?}.", p1(pairs.clone()));
+    println!("Divisors product is {:?}", p2(pairs.clone()));
 }
 
 fn p1 (v: Vec<Item>) -> usize {
@@ -32,5 +33,15 @@ fn p1 (v: Vec<Item>) -> usize {
                 None
             }
         })
-        .sum::<usize>()
+        .sum()
+}
+
+fn p2 (mut v: Vec<Item>) -> usize {
+    let divisors = Pair::get_all("[[2]]\n[[6]]").unwrap().1;
+
+    v.sort();
+
+    println!("{:?}", divisors.iter().map(|x| format!("{x:?}")).collect::<Vec<String>>().join("\n"));
+
+    v.into_iter().enumerate().filter_map(|(index, item)| if divisors.contains(&item) { Some(index + 1) } else {None}).product()
 }
